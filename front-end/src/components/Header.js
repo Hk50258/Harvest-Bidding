0b5fa9ada4af0829a6ce300da445acb5300cf0f7
bhanupratap1810 <bhanupratapbd1@gmail.com>
2020-11-15 17:02:29 +0530

    bh

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -2,11 +2,22 @@ import React from "react";
 
 function Header() {
   return (
+    <div className="container-fluid">
     <header>
+    <div className="row">
+    <div className="col left-auto">
       <h1>
         Company Logo
       </h1>
+    </div>
+    <div className="col right-auto">
+        <a href="#">Home</a>
+        <a href="/AboutUs">About Us</a>
+        <a href="#">Contact</a>
+    </div>
+    </div>
     </header>
+    </div>
   );
 }
 
