9b7e8ba071c7c70b577497954141599f3e2ed00e
Hk50258 <hk50258@gmail.com>
2020-11-16 00:26:48 +0530

    Collapse

--- front-end/src/components/Note.js
+++ front-end/src/components/Note.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { Collapse } from 'reactstrap'
 
 function Note(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+
   function handleClick() {
     props.onBid(props.id);
+    toggle();
   }
 
   return (
     <div className="note text-center">
-      <img src={props.imgurl} ></img>
+      <img src={props.imgurl} onClick={toggle} ></img>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      
+      <Collapse isOpen={isOpen}>
       <TextField
           id="outlined-number"
           label="Bidding Price"
@@ -27,6 +34,7 @@ function Note(props) {
       <Button className="mt-2" variant="contained" color="primary" onClick={handleClick} >
         Bid
       </Button>
+      </Collapse>
     </div>
   );
 }
